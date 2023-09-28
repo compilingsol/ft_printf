c67a3b508daccbf0517e366b5d761970f8b370eb
compilingsol <124470903+compilingsol@users.noreply.github.com>
2023-09-28 19:10:17 +0200

    WIP

--- ft_printf.c
+++ ft_printf.c
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maria-sg <maria-sg@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/21 18:51:22 by maria-sg          #+#    #+#             */
+/*   Updated: 2023/09/28 17:17:43 by maria-sg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 #include <stdarg.h>
@@ -20,65 +31,92 @@
 
 // 1. Para escanear todos los argumentos. Lleno de if-else
 //  aca se usa va arg?
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+void	ft_putnbr(int n)
+{
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar('-');
+			n = -n;
+		}
+		if (n > 9)
+			ft_putnbr(n / 10);
+		write(1, &"0123456789"[n % 10], 1);
+	}
+}
  
- int(?) printarg(va_list arg, char c, size_t i) //por que seria un int? puede ser otra cosa? por que?
+ int	printarg(va_list args, char c) //por que seria un int? puede ser otra cosa? por que?
  {
- 	va_start char;
-  
+ 	int	chars;
+
+	chars = 0;  
     if (c == 'c')
-        ft_putchar_fd();
-    if (c == 's')
-        ft_putstr_fd();
-    if (c == 'd')
-        ft_putnbr_fd();
-    if (c == 'i')
-        ft_itoa();
-    if (c == 'p')
-        ft_putpointer();
-    if (c == 'u')
-        ft_putundec();
-    if (c == 'x')
-        ft_lowhex();
-    if (c == 'X')
-        ft_upphex();
-    if (c == '%')
-        ft_putperc();
+        ft_putchar(va_arg(args, int));
+    else if (c == 's')
+        ft_putstr(va_arg(args, char *));
+    else if (c == 'd')
+        ft_putnbr(va_arg(args, int));
+    else if (c == 'i')
+        ft_putnbr(va_arg(args, int));
+    else if (c == 'p')
+        ft_putpointer(va_arg(args, uintprt_t));
+    else if (c == 'u')
+        ft_putundec(va_arg(args, unsigned int));
+    else if (c == 'x')
+        ft_lowhex(va_arg(args, int));
+    else if (c == 'X')
+        ft_upphex(va_arg(args, int));
+    else if (c == '%')
+        ft_putchar('%');
     
-    return (0);
-
-
+    return (chars);
  }
-
-
-
-
-
 
 int ft_printf(const char *str, ...)
 {
-    va_list arg;
+    va_list args;
     size_t  i;
+	size_t	chars;
 
-    va_start(arg, s);
+    va_start(args, str);
     i = 0;
-    while (*str)
+	chars = 0;
+    while (str[i] != '/0')
     {
         if (str[i] == '%')
         {
-            /* code */
+            chars = chars + printarg(args, str[i + 1]);
+			i++;
         }
-        
-    }
-    
-
-
-
-    size_t len;
-
-    len = ft_strlen(str);
-
-    printarg(va_?, c)
-    
-    return (len);
-    
+		else
+		{
+			chars = chars + printarg(str[i]);
+		}
+		i++;
+	}
+	va_end(args);
+	return (chars);    
 }
---
+++ ft_putpointer.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putpointer.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maria-sg <maria-sg@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 17:05:26 by maria-sg          #+#    #+#             */
+/*   Updated: 2023/09/28 19:09:30 by maria-sg         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+//calculates the length of the hexadecimal representation of the pointer.
+int	ft_ptrlen(uintptr_t ptr)
+{
+	int	len;
+	
+	len = 0;
+	while (ptr)
+	{
+		ptr = ptr / 16;
+		len++;
+	}
+	return (len);
+}
+
+//converts the pointer value to a string of hexadecimal digits. 
+char	*ft_hexptrstr(uintptr_t ptr)
+{
+	// char *str;
+	// int	len;
+	// int	store;
+
+	// len = ft_ptrlen(ptr);
+	// str = (char *)malloc((len + 1) * sizeof(char));
+	// if (!str)
+	// 	return (NULL);
+	// str[len] = '\0';
+	// len = len - 1;
+	// while (len >= 0)
+	// {
+	// 	store = ptr % 16;
+	// 	if (store < 10)
+	// 		store = store + 48;
+	// 	else
+	// 		store = store + 87;
+	// 	str[len--] = store;
+	// 	ptr = ptr / 16;
+	// }	
+	// return (str);
+
+	
+	int	len;
+	char	*str;
+	int	digit;
+
+	len = sizeof(ptr) * 2;
+	str = (char *)malloc((len +1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	str[len] = '/0';
+	len = len - 1;
+	while (len >= 0)
+	{
+		digit= ptr & 0xF;
+		if (digit < 10)
+			str[len] = '0' + digit;
+		else
+			str[len] = 'a' + digit - 10;
+		ptr >>= 4;
+	}
+	return (str);	
+}
+
+int	ft_putpointer(uintptr_t ptr)
+{
+	char	*pointer;
+	int	len;
+	
+	if (ptr == 0)
+	{
+		ft_putstr("NULL"); //this might be wrong
+		return (1);
+	}
+	pointer = ft_hexptrstr(ptr);
+	len = ft_putstr("0x") + ft_putstr(pointer);
+	free(pointer);
+	return (len);
+}
